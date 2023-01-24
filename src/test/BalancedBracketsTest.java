67d035e77eeeacc2122fda1160fc05bc6a6157f2
John Emilio <jl.emilio1@gmail.com>
2023-01-24 12:54:16 -0500

    Added Unit Tests and fixed error in main code

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +10,33 @@ public class BalancedBracketsTest {
     //TODO: add tests here
     @Test
     public void emptyTest() {
-        assertEquals(true, true);
+        assertEquals(true, BalancedBrackets.hasBalancedBrackets(""));
     }
 
+    @Test
+    public void testOnlyBracketsReturnsTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void testStringWithNoBracketsReturnsTure () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Hello World"));
+    }
+    @Test
+    public void testStringWithMixedBalancedBracketsReturnsTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("H[ello W]or[ld]"));
+    }
+    @Test
+    public void testThatUnbalancedBracketsReturnsFalse () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[][[]"));
+    }
+    @Test
+    public void testStartingWithClosingBracketsReturnsFalse () {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void testMultipleSetsOfOpenBracketsThatCloseIsTrue () {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
 
 }
